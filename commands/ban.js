6cba95996a98485725d818f7ebb4df6fcac27633
AJ-DeGroot <alan.michael.degroot@gmail.com>
2018-05-23 02:48:46 -0400

    Bot Commands Required in Commands Folder. Ban, Kick, Report, ServerInfo, BotInfo

---
+++ commands/ban.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!bUser) return message.channel.send("Can't find user!");
+  let bReason = args.join(" ").slice(22);
+
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Requires Admin");
+  if(bUser.hasPermission("MANAGE_MEMBERS")) return message.channel.send("Can't ban Admin")
+
+  let banEmbed = new Discord.RichEmbed()
+  .setDescription("Ban")
+  .setColor("#bc0000")
+  .addField("Banned User", `${bUser} with ID: ${bUser.id}`)
+  .addField("Banned By", `<@${message.author.id}> with ID: ${message.author.id}`)
+  .addField("Banned In", message.channel)
+  .addField("Reason", bReason);
+
+  let banChannel = message.guild.channels.find(`name`, "incidents");
+  if(!banChannel) return message.channel.send("Can't find incidents channel.");
+
+  message.guild.member(bUser).ban(bReason);
+  banChannel.send(banEmbed);
+}
+
+module.exports.help = {
+  name: "ban"
+}
--- index.js
+++ index.js
@@ -1,50 +1,72 @@
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+bot.commands = new Discord.Collection();
 
+//Import Bot Commands
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded.`);
+    bot.commands.set(props.help.name, props);
+  });
+})
+//End Import Bot Commands
+
+//Log Bot Into Discord
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online`);
-  bot.user.setActivity('with Self')
+  bot.user.setActivity('with Self', {type: "PLAYING"})
 });
 
+//Bot Commands
 bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
-  
+
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
+
+  //Kick
+  if(cmd === `${prefix}kick`){
+    return;
+  }
+
+  //Ban
+  if(cmd === `${prefix}ban`){
+  return;
+  }
+
+  //Report
+  if(cmd === `${prefix}report`){
+    return;
+  }
+
+  //Server Info
   if(cmd === `${prefix}serverinfo`){
-
-    let sicon = message.guild.iconURL;
-    let serverembed = new Discord.RichEmbed()
-    .setDescription("Server Information")
-    .setColor("#15f153")
-    .setThumbnail(sicon)
-    .addField("Server Name", message.guild.name)
-    .addField("Created On", message.guild.createdAt)
-    .addField("You Joined", message.member.joinedAt)
-    .addField("Total Members", message.guild.memberCount);
-
-    return message.channel.send(serverembed);
+    return;
   }
 
+  //Bot Info
   if(cmd === `${prefix}botinfo`){
-
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
-
-      return message.channel.send(botembed);
-
+    return;
   }
-});
 
+});
+//End Bot Commands
 
 bot.login(botconfig.token);
